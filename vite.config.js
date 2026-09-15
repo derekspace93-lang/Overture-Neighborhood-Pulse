af70d11d808928834d7d2e545657b635132b4fa9
Oseyijotham <derekspace93@gmail.com>
2026-09-16 00:22:40 +0100

    Upload

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/TrainerIQ/" : "/",
+  base:
+    process.env.NODE_ENV === "production"
+      ? "/overture-neighborhood-pulse/"
+      : "/",
 });
