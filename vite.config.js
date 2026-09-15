628e6212af384f8a563c02c9e735f66a684fd2d1
Oseyijotham <derekspace93@gmail.com>
2026-09-16 00:23:16 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ export default defineConfig({
   plugins: [react()],
   base:
     process.env.NODE_ENV === "production"
-      ? "/overture-neighborhood-pulse/"
+      ? "/Overture-Neighborhood-Pulse/"
       : "/",
 });
